abec613cc798deb8a5fc59a970eba33e42d92dfd
rajmoham <rajmoham10@gmail.com>
2024-01-21 14:25:38 +0000

    Validate file input for data file

---
+++ app.py
@@ -0,0 +1,18 @@
+from csv_file_data import CSVFileData
+
+class Main:
+    def __init__(self):
+        """
+        Initializes the object
+        """
+        BRISTOL_FILE = "./data/air_quality_data.csv"
+        try:
+            air_data_obj = CSVFileData(BRISTOL_FILE)
+        except FileNotFoundError:
+            print(f"The file at location '{BRISTOL_FILE}' cannot be found.")
+        except Exception:
+            print(f"Input file is not a csv file.")
+
+        
+if __name__ == "__main__":
+    main = Main()
--- test.py
+++ test.py
@@ -4,10 +4,24 @@ from csv_file_data import CSVFileData
 BRISTOL_FILE_LOC = "./data/air_quality_data.csv"
 
 class TestCSVFileDataClass(unittest.TestCase):
+    """Test cases for CSVFileData class"""
+
     def test_csv_data_parsing(self):
+        """Test CSV data parsing functionality"""
         expected_result = [['a','b','c','d'],
                             ['e','f','g','h']]
         csv_file = CSVFileData("./data/test_data.csv")
         actual_result = csv_file.get_data()
         self.assertEqual(actual_result, expected_result,
                          "Data is not parsed correctly")
+        
+    def test_incorrect_file_type(self):
+        """Test raising of exception for incorrect file type"""
+        with self.assertRaises(Exception):
+            CSVFileData("Some_file.pdf")
+
+    def test_file_not_existing(self):
+        """Test raising of exception for file not existing"""
+        with self.assertRaises(FileNotFoundError):
+            CSVFileData("./data/does_not_exist.csv")
+
