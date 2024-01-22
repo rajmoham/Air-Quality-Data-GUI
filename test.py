09418c1bf6e1e5ccfd9595e1c8bc25f359f0b3b5
rajmoham <rajmoham10@gmail.com>
2024-01-22 21:40:30 +0000

    Add additional test cases for average aqi function

--- test.py
+++ test.py
@@ -36,3 +36,34 @@ class TestAverageAQIFunction(unittest.TestCase):
         actual_result = calculate_average_aqi(input_reading1, input_reading2)
         self.assertEqual(actual_result, expected_result,
                         f"Average for test data should be {expected_result}")
+        
+    def test_average_aqi_all_empty_inputs(self):
+        expected_result = 0
+
+        input_readings = [['    ', '    '] for _ in range(20)]
+        actual_result = calculate_average_aqi(input_readings[0], 
+                                              input_readings[1])
+        self.assertEqual(actual_result, expected_result,
+            f"Average for empty inputs should be {expected_result}")
+        
+    def test_average_aqi_no_inputs(self):
+        expected_result = 0
+        input_readings = []
+        actual_result = calculate_average_aqi(input_readings, input_readings)
+        self.assertEqual(actual_result, expected_result,
+                         f"Average for no inputs should be {expected_result}")
+
+    def test_average_aqi_with_actual_data(self):
+        expected_result = 27.80
+        readings = bristol_data.get_readings()
+        average = calculate_average_aqi(readings[0], readings[1])
+        actual_result = round(average, 2)
+        self.assertEqual(expected_result, actual_result,
+                        f"Average for data should be {expected_result}")
+
+    def test_average_aqi_one_column_empty(self):
+        input_data = [i for i in range(20)]
+        expected_result = 9.5
+        actual_result = calculate_average_aqi(input_data, [])
+        self.assertEqual(expected_result, actual_result,
+                         "Result for average AQI should be 9.5")
