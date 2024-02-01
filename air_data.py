36e09f468637cd31450eaa3bac3c6676695b7774
rajmoham <rajmoham10@gmail.com>
2024-02-01 13:54:06 +0000

    Extract location name information from file

--- air_data.py
+++ air_data.py
@@ -20,11 +20,22 @@ class AirData(CSVFileData):
         Extracts and parses various data from the inherited CSV file data.
         """
         self.extract_title()
+        self.extract_location_names()
         
 
     def extract_title(self):
+        """Extracts the title from the file"""
         self._title = self._file_data[0][0]
 
     def get_title(self):
         """Returns the title of the air quality data."""
         return self._title
+    
+    def extract_location_names(self):
+        """Extracts the two location names from the file"""
+        self._location_names = self._file_data[6][2:4]
+    
+    def get_location_names(self):
+        return self._location_names
+    
+    
