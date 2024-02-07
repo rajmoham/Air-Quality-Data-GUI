4ca037216e5620851723aef472450a816ae3ff47
rajmoham <rajmoham10@gmail.com>
2024-02-07 16:36:01 +0000

    Create GUI window

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 from csv_file_data import CSVFileData
 from air_data import AirData
+import tkinter as tk
 
 class Main:
     def __init__(self):
         """
-        Initializes the object
+        Initialises the Main object.
+        
+        Creates a GUI.
         """
         BRISTOL_FILE = "./data/air_quality_data.csv"
         try:
             air_data_obj = CSVFileData(BRISTOL_FILE)
+
+            self.render_window()
         except FileNotFoundError:
             print(f"The file at location '{BRISTOL_FILE}' cannot be found.")
         except Exception:
             print(f"Input file is not a csv file.")
 
-        
+    def render_window(self):
+        """Instantiates the main GUI window"""
+        self.root = tk.Tk()
+        self.root.title("Python GUI")
+        self.root.geometry("300x300")
+
+        title_label = tk.Label(text = "Python GUI")
+        title_label.pack()
+
+        self.root.mainloop()
+
 if __name__ == "__main__":
     main = Main()
