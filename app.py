9d34ccf473b443dfc17e0b5cd8ed04992878a82c
rajmoham <rajmoham10@gmail.com>
2024-02-11 19:40:31 +0000

    Write logic for calculating information to display

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from csv_file_data import CSVFileData
 from air_data import AirData
 import tkinter as tk
+from helper import *
 
 class Main:
     def __init__(self):
@@ -12,6 +13,27 @@ class Main:
         BRISTOL_FILE = "./data/air_quality_data.csv"
         try:
             air_data_obj = CSVFileData(BRISTOL_FILE)
+
+            self.locations = air_data_obj.get_location_names()
+
+            data = air_data_obj.get_data()
+            data = separate_data(data)
+            three_window_data1 = highest_three_point_sd(data[0])
+            three_window_data2 = highest_three_point_sd(data[1])
+            
+            sd_data1 = population_sd(
+                get_three_point_window_data(three_window_data1))
+            sd_data2 = population_sd(
+                get_three_point_window_data(three_window_data2))
+            
+            self.header = ['Date', 'Time']
+
+            if sd_data1 > sd_data2:
+                self.header.append(self.locations[0])
+                self.three_window_data = three_window_data1
+            else:
+                self.header.append(self.locations[1])
+                self.three_window_data = three_window_data2
 
             self.render_window()
         except FileNotFoundError:
