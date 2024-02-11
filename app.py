0a040681d0eb34660acf8191c70ab4f921a04232
rajmoham <rajmoham10@gmail.com>
2024-02-11 20:35:54 +0000

    Display information in table using Tkinter

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from csv_file_data import CSVFileData
 from air_data import AirData
 import tkinter as tk
+from tkinter import ttk
 from helper import *
 
 class Main:
@@ -26,8 +27,6 @@ class Main:
                 get_three_point_window_data(three_window_data1))
             sd_data2 = population_sd(
                 get_three_point_window_data(three_window_data2))
-            
-            print("3")
 
             self.header = ['Date', 'Time']
 
@@ -49,12 +48,40 @@ class Main:
         """Instantiates the main GUI window"""
         self.root = tk.Tk()
         self.root.title("Python GUI")
-        self.root.geometry("300x300")
+        self.root.geometry("700x400")
 
-        title_label = tk.Label(text = "Python GUI")
-        title_label.pack()
+        self.display_as_table(self.header,
+                            self.three_window_data, self.root)
 
         self.root.mainloop()
+
+    def display_as_table(self, headers, entries, window):
+        """Displays data in a table format using Treeview (tkinker)"""
+
+        if entries is None:
+            return
+
+        # Constants used for table styling
+        COL_WIDTH = 175
+        ANCHOR_POS = 'center'
+        ROW_HEIGHT = 25
+
+        # Styling for table
+        table = ttk.Treeview(window, columns=headers, show='headings')
+        table.heading(headers[0], text=headers[0])
+        table.heading(headers[1], text=headers[1])
+        table.heading(headers[2], text=headers[2])
+        table.column(headers[0], width=COL_WIDTH, anchor=ANCHOR_POS)
+        table.column(headers[1], width=COL_WIDTH, anchor=ANCHOR_POS)
+        table.column(headers[2], width=COL_WIDTH, anchor=ANCHOR_POS)
+        ttk.Style().configure('Treeview', rowheight=ROW_HEIGHT)
+        ttk.Style().configure('Treeview.Heading', font=('Helvetica bold', 14))
+        
+        table['height'] = len(entries)
+
+        for row in entries:
+            table.insert('', index='end', values=row)
+        table.pack()
 
 if __name__ == "__main__":
     main = Main()
