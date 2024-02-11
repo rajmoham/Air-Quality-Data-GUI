f87c34e531fed5de30d3d3b1e8cfb4fc27291f17
rajmoham <rajmoham10@gmail.com>
2024-02-11 21:37:57 +0000

    Refactor to show all information systematically

--- app.py
+++ app.py
@@ -17,47 +17,52 @@ class Main:
         Creates a GUI.
         """
         BRISTOL_FILE = "./data/air_quality_data.csv"
+        NUM_OF_DP = 3 #Decimal Places
+
         try:
             self.air_data_obj = AirData(BRISTOL_FILE)
 
+            # Get title dataset
+            self.title = self.air_data_obj.get_title()
+
+            # Get data to calculate and display average aqi
+            self.readings = self.air_data_obj.get_readings()
+            self.avg_aqi_diff = calculate_average_aqi(self.readings[0],
+                                                    self.readings[1])
+            self.avg_aqi_diff = round(self.avg_aqi_diff, NUM_OF_DP)
+
+            # Get data to use for three-point-window calculation
             self.timescale = self.air_data_obj.get_timescale()
             self.readings = self.air_data_obj.get_readings()
             self.locations = self.air_data_obj.get_location_names()
 
+            # Extract data and calculates three-point-window for ach location
             data = self.air_data_obj.get_data()
             data = separate_data(data)
             three_window_data1 = highest_three_point_sd(data[0])
             three_window_data2 = highest_three_point_sd(data[1])
 
+            self.header = ['Date', 'Time']
+
+            # Calculate population SD for each location 
             sd_data1 = population_sd(
                 get_three_point_window_data(three_window_data1))
             sd_data2 = population_sd(
                 get_three_point_window_data(three_window_data2))
 
-            self.header = ['Date', 'Time']
-
+            # Compares SD for each location and outputs higher SD window
             if sd_data1 > sd_data2:
                 self.header.append(self.locations[0])
                 self.three_window_data = three_window_data1
             else:
                 self.header.append(self.locations[1])
                 self.three_window_data = three_window_data2
+
+
         except FileNotFoundError:
             print(f"The file at location '{BRISTOL_FILE}' cannot be found.")
         except Exception:
             print(f"Something went wrong.")
-
-        self.start_application()
-
-    def start_application(self):
-        """Collects data needed to display in GUI and renders window"""
-        NUM_OF_DP = 3 #Decimal Places
-
-        self.title = self.air_data_obj.get_title()
-        self.readings = self.air_data_obj.get_readings()
-        self.avg_aqi_diff = calculate_average_aqi(self.readings[0],
-                                                  self.readings[1])
-        self.avg_aqi_diff = round(self.avg_aqi_diff, NUM_OF_DP)
 
         self.render_window()
 
@@ -65,9 +70,8 @@ class Main:
         """Instantiates the main GUI window"""
         self.root = tk.Tk()
         self.root.title("Python GUI")
-        self.root.geometry("800x600")
+        self.root.geometry("1000x800")
 
-        
         title_label = tk.Label(text = self.title,
                                font=('Helvetica bold', 20),
                                pady=20)
@@ -81,6 +85,7 @@ class Main:
             pady=8)
         avg_diff_label.pack()
 
+        # Display three-point-window in table format
         self.display_as_table(self.header,
                             self.three_window_data, self.root)
 
