277b6c1faadeae32cc842f0d77dca0a0c739ff2c
rajmoham <rajmoham10@gmail.com>
2024-02-13 05:06:46 +0000

    Reformat AirData class to reduce number of functions, Add docstrings, Format to follow PEP

--- air_data.py
+++ air_data.py
@@ -2,88 +2,51 @@ from csv_file_data import CSVFileData
 
 
 class AirData(CSVFileData):
-    """
-    A subclass of CSVFileData for handling and extracting air quality
+    """A subclass of CSVFileData for handling and extracting air quality
     data from a CSV file.
     """
 
     def __init__(self, file):
-        """
-        Initialise the AirData object.
-
-        Invoke the constructor of the superclass (CSVFileData) and then
-        extracts air quality data.
-        """
+        """Initialise the AirData object."""
 
         super().__init__(file)
-        self._extract_information()
-
-    def _extract_information(self):
-        """Extract and parse various data from the inherited CSV file data.
-        """
-
-        self._extract_title()
-        self._extract_location_names()
-        self._extract_readings()
-
-    def _extract_title(self):
-        """Extract the title from the file"""
-
-        self._title = self._file_data[0][0]
 
     def get_title(self):
         """Return the title of the air quality data."""
 
-        return self._title
-
-    def _extract_location_names(self):
-        """Extract the two location names from the file"""
-
-        self._location_names = self._file_data[6][2:4]
+        return self._file_data[0][0]
 
     def get_location_names(self):
         """Return the two location names of the air quality data"""
 
-        return self._location_names
+        return self._file_data[6][2:4]
 
     def get_data(self):
-        """Override the function from the parent class
-        Extracts and Returns the data fr the air quality dat
+        """Extract and Return the data for the air quality data
+        (Overrides the function from the parent class)
         """
-        
-        # Creates deep copy of the file data
+
+        # Create deep copy of the file data
         data = [x[:] for x in self._file_data][9:]
 
-        for i,row in enumerate(data):
+        for i, row in enumerate(data):
             data[i][2] = self.cast_to_num(row[2])
             data[i][3] = self.cast_to_num(row[3])
 
         return data
 
-    def _extract_readings(self):
-        """Extract and parse the raw data readings from the file"""
-
+    def get_readings(self):
+        """Return readings from file data in separating columns."""
         reading1 = []
         reading2 = []
         for row in self.get_data():
             reading1.append((row[2]))
             reading2.append((row[3]))
 
-        self._readings = [reading1, reading2]
-
-    def get_readings(self):
-        return self._readings
-
-    def parse_data(self, data_field):
-        """Convert the given data field into float if it is not empty"""
-
-        if data_field.isspace():
-            return data_field
-
-        return float(data_field)
+        return reading1, reading2
 
     def get_timescale(self):
-        """Returns the timestamps for the readings."""
+        """Return the timestamps for the readings."""
         data = self.get_data()
         timescale = []
 
@@ -91,10 +54,18 @@ class AirData(CSVFileData):
             timescale.append(row[1])
 
         return timescale
-    
-    def cast_to_num(self,data_field):
-        """Converts the given data field into float if it is not empty"""
+
+    def parse_data(self, data_field):
+        """Convert the given data field into float if it is not empty."""
+
         if data_field.isspace():
             return data_field
-        
+
+        return float(data_field)
+
+    def cast_to_num(self, data_field):
+        """Convert the given data field into float if it is not empty."""
+        if data_field.isspace():
+            return data_field
+
         return float(data_field)
